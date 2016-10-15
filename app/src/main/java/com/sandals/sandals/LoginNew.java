0f865224abd584404748b60f962edaab61f717ed
scho3 <scho379@gmail.com>
2016-10-15 11:08:59 -0500

    Anthony - New Login Page

---
+++ app/src/main/java/com/sandals/sandals/LoginNew.java
@@ -0,0 +1,13 @@
+package com.sandals.sandals;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LoginNew extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_new);
+    }
+}
