8e38b3dfb1aebf4fc0793b0669f0f53cc3e8aa12
Kevin Lin <kevlin97@gmail.com>
2016-10-15 09:08:54 -0500

    Update RegisterActivity button and fields

--- app/src/main/java/com/sandals/sandals/RegisterActivity.java
+++ app/src/main/java/com/sandals/sandals/RegisterActivity.java
@@ -1,7 +1,9 @@
 package com.sandals.sandals;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -9,5 +11,16 @@ public class RegisterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register2);
+        Button regButton = (Button) findViewById(R.id.buttonRegister);
+        regButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                attemptRegister();
+            }
+        });
+    }
+
+    private void attemptRegister() {
+        // register
     }
 }
