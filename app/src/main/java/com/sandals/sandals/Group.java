32f4be7e9bbf3707e5175f9377234a0db4f8952d
Casey Takeda <kaze@berkeley.edu>
2016-10-15 12:44:45 -0500

    Made a Group Activity

--- app/src/main/java/com/sandals/sandals/Group.java
+++ app/src/main/java/com/sandals/sandals/Group.java
@@ -3,12 +3,31 @@ package com.sandals.sandals;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.os.Bundle;
+import android.os.StrictMode;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.EditText;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.io.StringWriter;
+import java.io.Writer;
+import java.net.URI;
+import java.net.URISyntaxException;
 
 public class Group extends AppCompatActivity {
 
@@ -20,6 +39,8 @@ public class Group extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        FloatingActionButton group = (FloatingActionButton) findViewById(R.id.create);
+
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -47,6 +68,75 @@ public class Group extends AppCompatActivity {
             }
         });
 
+        group.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(Group.this);
+                builder.setTitle(R.string.import_from_group_me);
+
+                builder.setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        // User wants to import!
+                        HttpResponse response = null;
+                        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+                        StrictMode.setThreadPolicy(policy);
+                        try {
+                            HttpClient client = new DefaultHttpClient();
+                            HttpGet request = new HttpGet();
+
+                            // Casey: 4c87fed074db013488c2001f5e6f38da
+                            // Kevin: kQeWcJ58iUzUc3pjB67MB7ClKTwrjSAwpkXVJ9SO
+                            request.setURI(new URI("https://api.groupme.com/v3/groups?token=kQeWcJ58iUzUc3pjB67MB7ClKTwrjSAwpkXVJ9SO"));
+                            response = client.execute(request);
+                            String data = convertStreamToString(response.getEntity().getContent());
+                            JSONObject json = new JSONObject(data);
+                            System.out.println(json);
+
+                        } catch (URISyntaxException e) {
+                            e.printStackTrace();
+                        } catch (ClientProtocolException e) {
+                            e.printStackTrace();
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                });
+
+                builder.setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        // User cancelled the dialog
+
+                    }
+                });
+
+                AlertDialog alertDialog = builder.create();
+                alertDialog.show();
+
+            }
+        });
+
+    }
+
+    public static String convertStreamToString(InputStream inputStream) throws IOException {
+        if (inputStream != null) {
+            Writer writer = new StringWriter();
+
+            char[] buffer = new char[1024];
+            try {
+                Reader reader = new BufferedReader(new InputStreamReader(inputStream, "UTF-8"),1024);
+                int n;
+                while ((n = reader.read(buffer)) != -1) {
+                    writer.write(buffer, 0, n);
+                }
+            } finally {
+                inputStream.close();
+            }
+            return writer.toString();
+        } else {
+            return "";
+        }
     }
 
 }
