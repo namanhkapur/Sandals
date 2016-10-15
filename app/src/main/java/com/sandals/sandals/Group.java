7c74c50d5163cf739b288525b77883c2f9192531
Casey Takeda <kaze@berkeley.edu>
2016-10-15 12:57:20 -0500

    Build dependecies

--- app/src/main/java/com/sandals/sandals/Group.java
+++ app/src/main/java/com/sandals/sandals/Group.java
@@ -16,6 +16,7 @@ import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -28,6 +29,11 @@ import java.io.StringWriter;
 import java.io.Writer;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
 
 public class Group extends AppCompatActivity {
 
@@ -90,6 +96,12 @@ public class Group extends AppCompatActivity {
                             response = client.execute(request);
                             String data = convertStreamToString(response.getEntity().getContent());
                             JSONObject json = new JSONObject(data);
+                            Map<String, Object> retMap = new HashMap<String, Object>();
+
+                            if(json != JSONObject.NULL) {
+                                retMap = toMap(json);
+                            }
+
                             System.out.println(json);
 
                         } catch (URISyntaxException e) {
@@ -137,6 +149,42 @@ public class Group extends AppCompatActivity {
         } else {
             return "";
         }
+    }
+
+    public static Map<String, Object> toMap(JSONObject object) throws JSONException {
+        Map<String, Object> map = new HashMap<>();
+
+        Iterator<String> keysItr = object.keys();
+        while(keysItr.hasNext()) {
+            String key = keysItr.next();
+            Object value = object.get(key);
+
+            if(value instanceof JSONArray) {
+                value = toList((JSONArray) value);
+            }
+
+            else if(value instanceof JSONObject) {
+                value = toMap((JSONObject) value);
+            }
+            map.put(key, value);
+        }
+        return map;
+    }
+
+    public static List<Object> toList(JSONArray array) throws JSONException {
+        List<Object> list = new ArrayList<Object>();
+        for(int i = 0; i < array.length(); i++) {
+            Object value = array.get(i);
+            if(value instanceof JSONArray) {
+                value = toList((JSONArray) value);
+            }
+
+            else if(value instanceof JSONObject) {
+                value = toMap((JSONObject) value);
+            }
+            list.add(value);
+        }
+        return list;
     }
 
 }
