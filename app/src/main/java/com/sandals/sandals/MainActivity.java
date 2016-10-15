7ae860c3d3a04036bc78890443ce689784220192
Kevin Lin <kevlin97@gmail.com>
2016-10-15 08:51:26 -0500

    Handle which activity to start based on login and registration state

--- app/src/main/java/com/sandals/sandals/MainActivity.java
+++ app/src/main/java/com/sandals/sandals/MainActivity.java
@@ -1,34 +1,35 @@
 package com.sandals.sandals;
 
-import android.app.AlertDialog;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent transitionToGroups = new Intent(MainActivity.this, Group.class);
-                startActivity(transitionToGroups);
+        boolean isRegistered = getSharedPreferences("isRegistered", 0).getBoolean("isRegistered", false);
+        boolean isLoggedIn = getSharedPreferences("isLoggedIn", 0).getBoolean("isLoggedIn", false);
+        if (isRegistered) {
+            if (isLoggedIn) {
+                Intent goToGroup = new Intent(MainActivity.this, Group.class);
+                startActivity(goToGroup);
+            } else {
+                Intent goToLogIn = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(goToLogIn);
             }
-        });
+        } else {
+            Intent goToRegister = new Intent(MainActivity.this, RegisterActivity.class);
+            startActivity(goToRegister);
+        }
     }
 
     @Override
